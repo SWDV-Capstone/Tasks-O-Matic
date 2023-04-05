2bb1d348fc302953e70e5804087c63ef6584cbac
Zand76 <37522349+Zand76@users.noreply.github.com>
2023-04-05 09:38:41 -0400

    Edit navigation

--- Tasks-O-Matic/src/views/FolderPage.vue
+++ Tasks-O-Matic/src/views/FolderPage.vue
@@ -5,21 +5,36 @@
         <ion-buttons slot="start">
           <ion-menu-button color="primary"></ion-menu-button>
         </ion-buttons>
-        <ion-title>{{ $route.params.id }}</ion-title>
+        <ion-title>Your {{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">{{ $route.params.id }}</ion-title>
+          <ion-title size="large">Your {{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <div id="container">
+      <!-- <div id="container">
         <strong class="capitalize">{{ $route.params.id }}</strong>
         <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div> -->
+
+      <div>
+        <ion-list>
+          <ion-item>
+            <ion-label>Content 1</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>Content 2</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>Content 3</ion-label>
+          </ion-item>
+        </ion-list>
       </div>
+
     </ion-content>
   </ion-page>
 </template>
