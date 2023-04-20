eb7a015c8a929357a14024869679febb28849836
Zand76 <37522349+Zand76@users.noreply.github.com>
2023-04-20 14:50:09 -0400

    attempt ios and android

---
+++ Tasks-O-Matic/ios/App/App/public/assets/TestPage-069bacec.js
@@ -0,0 +1,1 @@
+import{H as o}from"./PageHeader-955b1da0.js";import{d as s,c as n,w as e,u as a,o as r,a as t,i as c,I as l,e as u}from"./index-724ac2b0.js";const i=s({__name:"TestPage",setup(_){return(d,f)=>(r(),n(a(u),null,{default:e(()=>[t(o),t(a(l),null,{default:e(()=>[c("Hello Test Page! ")]),_:1})]),_:1}))}});export{i as default};
